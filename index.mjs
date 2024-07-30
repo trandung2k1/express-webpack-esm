483246795bd2be9a8947fdf2de39e04a7d064823
trandung2k1 <trandungitksnb2001@gmail.com>
2024-07-30 15:09:15 +0700

    init commit

---
+++ index.mjs
@@ -0,0 +1,42 @@
+console.log('Main');
+import 'dotenv/config';
+import express from 'express';
+import colors from 'colors';
+import { createClient } from 'redis';
+import connectDB from './db.mjs';
+const port = process.env.PORT || 3000;
+const redisClient = createClient({});
+redisClient.on('error', (err) => {
+    console.log(err);
+    console.log(colors.red('Redis Client Error'));
+    process.exit(1);
+});
+redisClient.on('connect', () => {
+    console.log(colors.green('Redis plugged in.'));
+});
+await redisClient.connect();
+// await connectDB()
+const startServer = () => {
+    const app = express();
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.get('/', (req, res) => {
+        throw new Error('Haha');
+    });
+    app.get('/ok', (req, res) => {
+        return res.send('Hello, world');
+    });
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    }).on('error', (e) => {
+        console.log(`Error: ${e.message}`);
+    });
+};
+
+connectDB()
+    .then(() => {
+        startServer();
+    })
+    .catch((err) => {
+        console.log(`Error: ${err.message}`);
+    });
